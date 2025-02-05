253bb370476feee29b46444232576a29ba4cd4e4
David Baas <Dcbaas@users.noreply.github.com>
2025-02-05 00:56:58 -0500

    Think I have config working

--- baas_backup_util/src/config.cpp
+++ baas_backup_util/src/config.cpp
@@ -5,6 +5,8 @@ using json = nlohmann::json;
 
 #include <spdlog/spdlog.h>
 
+#include <concepts>
+#include <optional>
 #include <fstream>
 #include <sstream>
 
@@ -15,6 +17,19 @@ namespace baas::configuration
     constexpr std::string_view path = "path";
     constexpr std::string_view recursive = "recursive";
     constexpr std::string_view outputs = "outputs";
+
+    template <typename T>
+    concept ExceptionType = std::derived_from<T, std::exception>;
+
+    template <typename T, ExceptionType E>
+    T get_or_throw(const json& j, const std::string& key, const E& exception)
+    {
+        if (j.contains(key))
+        {
+            return j.at(key).get<T>();
+        }
+        throw exception;
+    }
 
 
     Config read_config(const std::string& file_path)
@@ -27,27 +42,45 @@ namespace baas::configuration
             throw ConfigFileNotFound(file_path);
         }
         config_json = json::parse(json_file);
-        // TODO What happens if this value isn't present in the json? We need to throw an exception for that
-        // TODO I need a holistic solution to check for values that may not be present in the Json input. 
-        config.seven_zip_exec = config_json.at(std::string(seven_zip_exec)).get<std::string>();
-        if (config.seven_zip_exec.empty())
+
+        config.seven_zip_exec = get_or_throw<std::string>(config_json, 
+            std::string(seven_zip_exec), 
+            BadConfiguration("7zipExec value is missing in the configuration file.")
+        );
+
+        if (!config_json.contains(std::string(inputs)))
         {
-            throw std::runtime_error("7zipExec value is missing in the configuration file.");
+            throw BadConfiguration("No input paths found in the configuration file.");
         }
 
         for (const auto& input : config_json.at(std::string(inputs)))
         {
-            std::string input_path = input.at(std::string(path)).get<std::string>();
-            bool is_recursive = input.at(std::string(recursive)).get<bool>();
+            // std::string input_path = input.at(std::string(path)).get<std::string>();
+            std::string input_path = get_or_throw<std::string>(input, 
+                std::string(path), 
+                BadConfiguration("Input list contains entry where path is missing.")
+            );
+            bool is_recursive{false};
+            if (input.contains(std::string(recursive)))
+            {
+                is_recursive = input.at(std::string(recursive)).get<bool>();
+            }
             config.inputs.push_back({input_path, is_recursive});
         }
 
+
+        // Is this redundant? Probably
         if (config.inputs.empty())
         {
             throw BadConfiguration("No input paths found in the configuration file.");
         }
 
         // TODO need to implement outputs
+        if (!config_json.contains(std::string(outputs)))
+        {
+            throw BadConfiguration("No output paths found in the configuration file.");
+        }
+
         for (const auto& output : config_json.at(std::string(outputs)))
         {
             config.outputs.push_back(output.get<std::string>());
@@ -61,6 +94,7 @@ namespace baas::configuration
         return config;
     }
 
+
     void Config::display_info() const
     {
         namespace logger = spdlog;
@@ -68,11 +102,11 @@ namespace baas::configuration
         oss << "7zip Executable: " << seven_zip_exec << "\n";
         for (const auto& input : inputs)
         {
-            oss << "Input Path: " << input.path << "\n";
+            oss << '\t' << "Input Path: " << input.path << "\n";
         }
         for (const auto& output : outputs)
         {
-            oss << "Output Path: " << output << "\n";
+            oss << '\t' << "Output Path: " << output << "\n";
         }
         logger::info(oss.str());
     }
